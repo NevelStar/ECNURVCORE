5009833cfffc43b9eefd881ee54d751e60e34061
MarrynDelt <1072024185@qq.com>
2021-07-14 23:53:34 +0800

    type U supported

--- RTL/1.SingleCycle/ex/alu_jmp.v
+++ RTL/1.SingleCycle/ex/alu_jmp.v
@@ -2,6 +2,7 @@
 //Single Cycle CPU
 //Created by Chesed
 //2021.07.12
+//Edited in 2021.07.14
 
 module alu_jmp(
 
@@ -10,6 +11,7 @@ module alu_jmp(
 	input	[31:0]		jmp 		,
 
 	input	[31:0]		imm_ext		,
+	input	[31:0]		imm_u_ext	,
 
 
 	output reg	[31:0]	jmp_to
@@ -19,10 +21,11 @@ module alu_jmp(
 
 
 	always@(*) begin
-		case(operation[3:2])
-			2'b11:		jmp_to <= jmp << 2;						//J jal
-			2'b01:		jmp_to <= data_reg + (imm_ext << 2);	//I jalr
-			2'b00:		jmp_to <= imm_ext << 2;					//Type-B
+		case(operation[4:2])
+			3'b101:		jmp_to <= imm_u_ext;				//U auipc
+			3'b011:		jmp_to <= jmp;						//J jal 
+			3'b001:		jmp_to <= data_reg + imm_ext;		//I jalr
+			3'b000:		jmp_to <= imm_ext ;					//Type-B
 			default:	jmp_to <= 32'd0;
 		endcase
 	end
--- RTL/1.SingleCycle/ex/alu_mem.v
+++ RTL/1.SingleCycle/ex/alu_mem.v
@@ -13,8 +13,7 @@ module alu_mem(
 	output 	[31:0]		addr_mem
 
 );
-	wire [31:0] offset_addr;
-	assign offset_addr = offset << 2;
-	assign addr_mem = data_reg + offset_addr;
+
+	assign addr_mem = data_reg + offset;
 
 endmodule
