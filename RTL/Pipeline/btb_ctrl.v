95ce29da7d29235e473600064774ff65bb9ea35a
MarrynDelt <1072024185@qq.com>
2021-08-02 21:51:37 +0800

    branch target buffer

--- RTL/Pipeline/btb_ctrl.v
+++ RTL/Pipeline/btb_ctrl.v
@@ -8,6 +8,7 @@ module ctrl_btb(
 	input						rst_n				,
 	
 	input	[`BUS_ADDR_MEM]		pc_i				,
+	input	[`BUS_ADDR_MEM]		pc_jmp_i			,
 	input	[`BUS_ADDR_MEM]		target_pc_i			,
 	input						record_en_i			,
 	
@@ -22,35 +23,30 @@ module ctrl_btb(
 	reg [`BUS_ADDR_MEM] target_buffer;
 
 	reg jmp_prediction_t;
-	reg prediction_error;
 
 	assign target_pc_o = (jmp_prediction == `JMP_EN) ? target_buffer : `MEM_ADDR_ZERO;
 	assign prediction_o = (pc_i == pc_buffer) ? `JMP_EN : `JMP_DIS;
-	assign prediction_error_o = prediction_error;
+	assign prediction_error_o = (jmp_prediction_t == record_en_i) ? `JMP_ERIGHT : `JMP_ERROR;
 	assign prediction_t_o = jmp_prediction_t;
 
 	always@(posedge clk or negedge rst_n) begin
 		if(!rst_n) begin
 			pc_buffer <= `MEM_ADDR_ZERO;
 			target_buffer <= MEM_ADDR_ZERO;
-			prediction_error <= `JMP_RIGHT;
 		end
 		else begin
 			case({jmp_prediction_t,record_en_i})
 				2'b10: begin
 					pc_buffer <= `MEM_ADDR_ZERO;
 					target_buffer <= `MEM_ADDR_ZERO;
-					prediction_error <= `JMP_ERROR;
 				end
 				2'b01: begin
-					pc_buffer <= pc_i;
+					pc_buffer <= pc_jmp_i;
 					target_buffer <= target_pc_i;
-					prediction_error <= `JMP_ERROR;
 				end
 				default: begin
 					pc_buffer <= pc_buffer;
 					target_buffer <= target_buffer;
-					prediction_error <= `JMP_RIGHT;
 				end
 			endcase
 		end
