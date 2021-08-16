1c8dc070ca0f05c69d78aab6072772c3dd4ece0f
MarrynDelt <1072024185@qq.com>
2021-08-16 18:25:25 +0800

    csr decode

--- RTL/Pipeline/ex_stage.v
+++ RTL/Pipeline/ex_stage.v
@@ -87,7 +87,7 @@ module ex_stage(
 		.data_reg_wr_o 	(data_reg_wr_o),
 		.reg_wr_en_o	(reg_wr_en_o)	
 	);
- /*
+ 
 	ex_csr U_ex_csr
 	(
 		.clk			(clk),
@@ -122,6 +122,6 @@ module ex_stage(
 		.mtie_o			(),
 		.glb_irq_o		()
 	);
-	*/
+	
 
 endmodule
